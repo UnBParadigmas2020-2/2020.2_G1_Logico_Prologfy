77eab214298ba8dd3c70092598b573b92f49de4a
Daniel Marques <danielmarques7@hotmail.com>
2021-03-26 09:29:47 -0300

    Move musica facts to another file

---
+++ musics.pl
@@ -0,0 +1,20 @@
+table(musica(nome : atom, artista : atom, genero : atom)).
+
+:- dynamic musica/3.
+
+musica(musica1, ariana, dance).
+musica(musica2, ariana, romantico).
+musica(musica3, ivete, axe).
+musica(musica4, drake, hip-hop).
+musica(musica5, ivete, dance).
+musica(musica6, ivete, romantico).
+musica(musica7, 'wesley safadao', forro).
+musica(musica8, ivete, dance).
+musica(musica9, 'wesley safadao', forro).
+musica(musica10, ariana, romantico).
+musica(musica11, drake, hip-hop).
+musica(musica12, ariana, dance).
+musica(musica13, drake, rap).
+musica(musica14, ivete, axe).
+musica(musica15, ivete, dance).
+musica(musica16, drake, rap).
