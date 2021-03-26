740d743404f7c9c7edee81f99debc4eea06961af
Daniel Marques <danielmarques7@hotmail.com>
2021-03-26 11:02:22 -0300

    Merge branch 'basico' into feature/album

--- Prologfy.pl
+++ Prologfy.pl
@@ -1,30 +1,7 @@
-:- dynamic artista/3.
-
-artista(ariana, [dance, romantico], solo).
-artista(ivete, [axe, romantico], solo).
-artista(drake, [rap, hip-hop], solo).
-artista('wesley safadao', [forro, romantico], solo).
-
-
-:- dynamic musica/3.
-
-musica(musica1, ariana, dance).
-musica(musica2, ariana, romantico).
-musica(musica3, ivete, axe).
-musica(musica4, drake, hip-hop).
-musica(musica5, ivete, dance).
-musica(musica6, ivete, romantico).
-musica(musica7, 'wesley safadao', forro).
-musica(musica8, ivete, dance).
-musica(musica9, 'wesley safadao', forro).
-musica(musica10, ariana, romantico).
-musica(musica11, drake, hip-hop).
-musica(musica12, ariana, dance).
-musica(musica13, drake, rap).
-musica(musica14, ivete, axe).
-musica(musica15, ivete, dance).
-musica(musica16, drake, rap).
-
+:- [artists].
+:- [albums].
+:- [musics].
+:- [api].
 
 musicasGenero(Genero):-
 	findall(Musica,musica(Musica, _, Genero),Lista),
@@ -53,3 +30,4 @@ artistasGenero(Especifico):-
 percorreLista(Especifico, H):- Especifico == H.
 percorreLista(Especifico, [H|_]):- Especifico == H.
 percorreLista(Especifico, [_|T]):- percorreLista(Especifico, T).
+
