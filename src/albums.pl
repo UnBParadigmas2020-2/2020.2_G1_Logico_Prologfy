afaa1f01e090e3100680603fe9646d54bbbf0307
Elias Bernardo <ebmm01@gmail.com>
2021-03-27 19:41:47 -0300

    Moving facts to db/ folder
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Fernando Miranda <ilusdasilva@gmail.com>
    Co-authored-by: Daniel Marques <danielmarques7@hotmail.com>

--- src/albums.pl
+++ src/albums.pl
@@ -3,14 +3,6 @@
 
 :- dynamic album/3.
 
-album('Damn', 'Kendrick Lamar', '2017').
-album('Lemonade', 'Beyoncé', '2016').
-album('Blonde', 'Frank Ocean', '2016').
-album('Ctrl', 'SZA', '2017').
-album('The Life of Pablo', 'Kanye West', '2016').
-album('Uma Nova História', 'wesley safadao', '2012').
-album('Paradise', 'Wesley Safadao', '2014').
-
 addAlbum:-
     read_atom_with_message('Nome', AlbumName),
     read_atom_with_message('Autor', ArtistName),
--- src/musics.pl
+++ src/musics.pl
@@ -4,17 +4,17 @@
 :- dynamic music/3.
 
 % nome, gênero, ano, álbum, 
-music('Não pare', [forro, romantico], 'Uma Nova História').
-music('Disco voador', [forro, romantico], 'Uma Nova História').
-music('Caranguejo', [forro, romantico], 'Uma Nova História').
-music('Ei olha o som', [forro, romantico], 'Uma Nova História').
-music('Vai esperar', [forro, romantico], 'Uma Nova História').
-music('Monotonia', [forro, romantico], 'Paradise').
-music('Poderosa', [forro, romantico], 'Paradise').
+% music('Não pare', [forro, romantico], 'Uma Nova História').
+% music('Disco voador', [forro, romantico], 'Uma Nova História').
+% music('Caranguejo', [forro, romantico], 'Uma Nova História').
+% music('Ei olha o som', [forro, romantico], 'Uma Nova História').
+% music('Vai esperar', [forro, romantico], 'Uma Nova História').
+% music('Monotonia', [forro, romantico], 'Paradise').
+% music('Poderosa', [forro, romantico], 'Paradise').
 
 addMusic:-
     read_atom_with_message('Nome', MusicName),
-    read_atom_list('Genero(s) (separados por vírgula caso > 1)', ',', Generos),
+    read_atom_with_message('Genero', Generos),
     read_atom_with_message('Album', AlbumName),
     insertInto('music', [MusicName,Generos,AlbumName], _).
 
